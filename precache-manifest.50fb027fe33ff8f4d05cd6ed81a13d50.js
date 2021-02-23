a62a76071e3f874a514ed5e691e3bf1736a1bfad
asma496 <asmaashraf496@gmail.com>
2021-02-23 15:44:59 +0500

    Updates

---
+++ precache-manifest.50fb027fe33ff8f4d05cd6ed81a13d50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7f4f12d315c8492fd61bb0d7dc88be",
+    "url": "/Firebase-react-auth/index.html"
+  },
+  {
+    "revision": "9dc43e0d1084dcb4161e",
+    "url": "/Firebase-react-auth/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "9dc43e0d1084dcb4161e",
+    "url": "/Firebase-react-auth/static/js/2.296f905e.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/Firebase-react-auth/static/js/2.296f905e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c596967c940efb511b5c",
+    "url": "/Firebase-react-auth/static/js/main.83c0b495.chunk.js"
+  },
+  {
+    "revision": "602f3593cb801cda3919",
+    "url": "/Firebase-react-auth/static/js/runtime-main.1f3880ff.js"
+  }
+]);
